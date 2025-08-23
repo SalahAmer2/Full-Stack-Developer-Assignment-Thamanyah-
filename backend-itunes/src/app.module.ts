b927ac722ab802e7b7a0f5a8b7f4da682323cf38
SalahAmer2 <salahayman2@gmail.com>
2025-08-23 09:23:14 +0300

    fixed queue.map error

--- backend-itunes/src/app.module.ts
+++ backend-itunes/src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { QueueModule } from './queue/queue.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { RecentsModule } from './recents/recents.module';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +10,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule, AuthModule, ConfigModule.forRoot({isGlobal:true})],
+  imports: [QueueModule, FavoritesModule, RecentsModule, HttpModule, AuthModule, ConfigModule.forRoot({isGlobal:true})],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- backend-itunes/src/auth/auth.module.ts
+++ backend-itunes/src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport'; //newly added from code to fix queue error
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma.service';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '7d' } })],
+  imports: [PassportModule, JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '7d' } })],
   controllers: [AuthController],
   providers: [AuthService, PrismaService, JwtStrategy],
   exports: [AuthService],
